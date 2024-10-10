1074bd267ece2e41bc5c60337626ef45e6a1d057
Abbie De Zwart <abbie.dezwart@unrvld.com>
2024-10-10 11:24:28 +0100

    Adjectives

---
+++ dist/js/data/adjectives.js
@@ -0,0 +1,221 @@
+const data = 
+{
+	"adjectives": [
+		{
+			"espanol": "delgado",
+			"english": "thin"
+		},
+		{
+			"espanol": "delgadísimo",
+			"english": "thin"
+		},
+		{
+			"espanol": "gordo",
+			"english": "fat"
+		},
+		{
+			"espanol": "gordisimo",
+			"english": "very fat"
+		},
+		{
+			"espanol": "alto",
+			"english": "high"
+		},
+		{
+			"espanol": "altísimo",
+			"english": "very high"
+		},
+		{
+			"espanol": "bajo",
+			"english": "low"
+		},
+		{
+			"espanol": "bajísimo",
+			"english": "very low"
+		},
+		{
+			"espanol": "lindo",
+			"english": "pretty"
+		},
+		{
+			"espanol": "lindísimo",
+			"english": "very pretty"
+		},
+		{
+			"espanol": "triste",
+			"english": "sad"
+		},
+		{
+			"espanol": "tristísimo",
+			"english": "very sad"
+		},
+		{
+			"espanol": "feliz",
+			"english": "happy"
+		},
+		{
+			"espanol": "felicísimo",
+			"english": "very happy"
+		},
+		{
+			"espanol": "fun",
+			"english": "divertido"
+		},
+		{
+			"espanol": "divertidísimo",
+			"english": "very fun"
+		},
+		{
+			"espanol": "aburrido",
+			"english": "boring"
+		},
+		{
+			"espanol": "abburidísimo",
+			"english": "very boring"
+		},
+		{
+			"espanol": "hermosa",
+			"english": "beautiful"
+		},
+		{
+			"espanol": "hermosísimo",
+			"english": "very beautiful"
+		},
+		{
+			"espanol": "horrible",
+			"english": "horrible"
+		},
+		{
+			"espanol": "horribilísimo",
+			"english": "very horrible"
+		},
+		{
+			"espanol": "ancho",
+			"english": "wide"
+		},
+		{
+			"espanol": "anchísimo",
+			"english": "very wide"
+		},
+		{
+			"espanol": "largo",
+			"english": "long"
+		},
+		{
+			"espanol": "larguísimo",
+			"english": "very long"
+		},
+		{
+			"espanol": "breve",
+			"english": "brief"
+		},
+		{
+			"espanol": "brevísimo",
+			"english": "very brief"
+		},
+		{
+			"espanol": "corto",
+			"english": "short"
+		},
+		{
+			"espanol": "cortísimo",
+			"english": "very short"
+		},
+		{
+			"espanol": "salado",
+			"english": "salty"
+		},
+		{
+			"espanol": "saladísimo",
+			"english": "very salty"
+		},
+		{
+			"espanol": "dulce",
+			"english": "sweet"
+		},
+		{
+			"espanol": "dulcísimo",
+			"english": "very sweet"
+		},
+		{
+			"espanol": "amargo",
+			"english": "bitter"
+		},
+		{
+			"espanol": "amaguísimo",
+			"english": "very bitter"
+		},
+		{
+			"espanol": "ácido",
+			"english": "acidic"
+		},
+		{
+			"espanol": "ácidísimo",
+			"english": "very acidic"
+		},
+		{
+			"espanol": "recto",
+			"english": "straight"
+		},
+		{
+			"espanol": "rectísimo",
+			"english": "very straight"
+		},
+		{
+			"espanol": "directo",
+			"english": "direct"
+		},
+		{
+			"espanol": "directísimo",
+			"english": "very direct"
+		},
+		{
+			"espanol": "grande",
+			"english": "big"
+		},
+		{
+			"espanol": "grandísimo",
+			"english": "very big"
+		},
+		{
+			"espanol": "pequeño",
+			"english": "small"
+		},
+		{
+			"espanol": "pequeñísimo",
+			"english": "very small"
+		},
+		{
+			"espanol": "lejano",
+			"english": "far"
+		},
+		{
+			"espanol": "lejanísimo",
+			"english": "very far"
+		},
+		{
+			"espanol": "cercano",
+			"english": "close"
+		},
+		{
+			"espanol": "cercanísimo",
+			"english": "very close"
+		},
+		{
+			"espanol": "rápido",
+			"english": "fast"
+		},
+		{
+			"espanol": "rapidísimo",
+			"english": "very fast"
+		},
+		{
+			"espanol": "lento",
+			"english": "slow"
+		},
+		{
+			"espanol": "lentísimo",
+			"english": "very slow"
+		},
+	]
+}
---
+++ dist/js/words.js
@@ -0,0 +1,80 @@
+var words = undefined;
+var myAnswer; 
+var spanishToEnglish;
+var lblWord;
+
+function initialiseWords(someWords) {
+	words = someWords;
+	myAnswer = document.getElementById("txtAnswer");
+	spanishToEnglish = document.getElementById("chkSpanish");
+	lblWord = document.getElementById("lblWord");
+	next();
+};
+
+function check() {
+	if (myAnswer.value === "") {
+		myAnswer.parentElement.classList.remove("correct");
+		myAnswer.parentElement.classList.remove("wrong");
+	} else {
+		myAnswer.value = myAnswer.value.toLowerCase(); // for mobile capitalisation
+		if (myAnswer.value.localeCompare(getAnswer()) == 0) {
+			myAnswer.parentElement.classList.add("correct");
+			myAnswer.parentElement.classList.remove("wrong");
+		} else {
+			myAnswer.parentElement.classList.remove("correct");
+			myAnswer.parentElement.classList.add("wrong");
+		}
+	}
+}
+
+function reveal() {
+	check();
+	myAnswer.parentElement.classList.remove("correct");
+	myAnswer.parentElement.classList.remove("wrong");
+	myAnswer.value = getAnswer();
+}
+
+function next() {
+	var index = Math.floor(Math.random() * words.length);
+	var thisWord = words[index];
+
+	if (thisWord == undefined) {
+		console.log(word + " " + index + " undefined!");
+	}
+	else {
+		myAnswer.value = "";
+		myAnswer.dataset.english = thisWord.english;
+		myAnswer.dataset.espanol = thisWord.espanol;
+		myAnswer.parentElement.classList.remove("correct");
+		myAnswer.parentElement.classList.remove("wrong");
+
+		if (spanishToEnglish.checked) {
+			lblWord.innerHTML = thisWord.espanol;
+		}
+		else {
+			lblWord.innerHTML = thisWord.english;
+		}
+	}
+}
+
+function insert(character) {
+	myAnswer.value +=character;
+	myAnswer.focus();
+}
+
+function swapLanguage() {
+	myAnswer.value = "";
+	myAnswer.parentElement.classList.remove("correct");
+	myAnswer.parentElement.classList.remove("wrong");
+
+	if (spanishToEnglish.checked) {
+		lblWord.innerHTML = myAnswer.dataset.espanol;
+	}
+	else {
+		lblWord.innerHTML = myAnswer.dataset.english;
+	}
+}
+
+function getAnswer() {
+	return spanishToEnglish.checked ? myAnswer.dataset.english : myAnswer.dataset.espanol;
+}
