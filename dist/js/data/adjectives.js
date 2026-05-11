ea66b1f4067d180552c8ba3e89c56fd3a297cf94
Abbie de Zwart <scarophion@yahoo.com>
2026-05-11 12:48:02 +0100

    All adjectives and homepage

--- dist/js/data/adjectives.js
+++ dist/js/data/adjectives.js
@@ -449,5 +449,449 @@ const data =
                     "english": "very tender"
                 },
             ]
+        },
+        {
+            "name": "adjectives 3",
+            "words": [
+                {
+                    "english": "agile",
+                    "espanol": "ágil"
+                },
+                {
+                    "english": "more agile",
+                    "espanol": "más ágil"
+                },
+                {
+                    "english": "extremely agile",
+                    "espanol": "ágilísimo"
+                },
+                {
+                    "english": "useful",
+                    "espanol": "útil"
+                },
+                {
+                    "english": "most useful",
+                    "espanol": "más útil"
+                },
+                {
+                    "english": "very useful",
+                    "espanol": "utilísimo"
+                },
+                {
+                    "english": "comical",
+                    "espanol": "cómico"
+                },
+                {
+                    "english": "more comical",
+                    "espanol": "más cómico"
+                },
+                {
+                    "english": "hilarious",
+                    "espanol": "comiquísimo"
+                },
+                {
+                    "english": "sadistic",
+                    "espanol": "sádico"
+                },
+                {
+                    "english": "more sadistic",
+                    "espanol": "más sádico"
+                },
+                {
+                    "english": "extremely sadistic",
+                    "espanol": "sadiquísimo"
+                },
+                {
+                    "english": "kind",
+                    "espanol": "bondadoso"
+                },
+                {
+                    "english": "kinder",
+                    "espanol": "más bondadoso"
+                },
+                {
+                    "english": "most kind",
+                    "espanol": "bondadosísimo"
+                },
+                {
+                    "english": "silly",
+                    "espanol": "tonto"
+                },
+                {
+                    "english": "dumber",
+                    "espanol": "más tonto"
+                },
+                {
+                    "english": "very stupid",
+                    "espanol": "tontísimo"
+                },
+                {
+                    "english": "good",
+                    "espanol": "bueno"
+                },
+                {
+                    "english": "better",
+                    "espanol": "más bueno"
+                },
+                {
+                    "english": "awesome",
+                    "espanol": "buenísimo"
+                },
+                {
+                    "english": "bad",
+                    "espanol": "malo"
+                },
+                {
+                    "english": "worse",
+                    "espanol": "más malo"
+                },
+                {
+                    "english": "lousy",
+                    "espanol": "malísimo"
+                },
+                {
+                    "english": "poor",
+                    "espanol": "pobre",
+                    "enDesc": "Skint."
+                },
+                {
+                    "english": "poorer",
+                    "espanol": "más pobre"
+                },
+                {
+                    "english": "extremely poor",
+                    "espanol": "paupérrimo"
+                },
+                {
+                    "english": "rich",
+                    "espanol": "rico"
+                },
+                {
+                    "english": "richer",
+                    "espanol": "más rico"
+                },
+                {
+                    "english": "delicious",
+                    "espanol": "riquísimo"
+                },
+                {
+                    "english": "mild",
+                    "espanol": "leve"
+                },
+                {
+                    "english": "milder",
+                    "espanol": "más leve"
+                },
+                {
+                    "english": "very slight",
+                    "espanol": "levísimo"
+                },
+                {
+                    "english": "delicate",
+                    "espanol": "delicado",
+                    "engDesc": "Fragile."
+                },
+                {
+                    "english": "more delicate",
+                    "espanol": "más delicado"
+                },
+                {
+                    "english": "extremely delicate",
+                    "espanol": "delicadísimo"
+                },
+                {
+                    "english": "ambitious",
+                    "espanol": "ambicioso"
+                },
+                {
+                    "english": "more ambitious",
+                    "espanol": "más ambicioso"
+                },
+                {
+                    "english": "extremely ambitious",
+                    "espanol": "ambiciosísimo"
+                },
+                {
+                    "english": "humble",
+                    "espanol": "humilde"
+                },
+                {
+                    "english": "more humble",
+                    "espanol": "más humilde"
+                },
+                {
+                    "english": "most humble",
+                    "espanol": "humildísimo"
+                },
+                {
+                    "english": "rough",
+                    "espanol": "áspero"
+                },
+                {
+                    "english": "rougher",
+                    "espanol": "más áspero"
+                },
+                {
+                    "english": "very rough",
+                    "espanol": "asperísimo"
+                },
+                {
+                    "english": "gentle",
+                    "espanol": "suave"
+                },
+                {
+                    "english": "softer",
+                    "espanol": "más suave"
+                },
+                {
+                    "english": "very soft",
+                    "espanol": "suavísimo"
+                },
+                {
+                    "english": "attentive",
+                    "espanol": "atento"
+                },
+                {
+                    "english": "more attentive",
+                    "espanol": "más atento"
+                },
+                {
+                    "english": "very attentive",
+                    "espanol": "atentÍsimo"
+                },
+                {
+                    "english": "sleepy",
+                    "espanol": "dormido"
+                },
+                {
+                    "english": "more sleepy",
+                    "espanol": "más dormido"
+                },
+                {
+                    "english": "fast asleep",
+                    "espanol": "dormidísimo"
+                },
+                {
+                    "english": "resigned",
+                    "espanol": "resignado"
+                },
+                {
+                    "english": "more resigned",
+                    "espanol": "más resignado"
+                },
+                {
+                    "english": "utterly resigned",
+                    "espanol": "resignadísimo"
+                },
+                {
+                    "english": "glad",
+                    "espanol": "contento"
+                },
+                {
+                    "english": "happier",
+                    "espanol": "más contento"
+                },
+                {
+                    "english": "delighted",
+                    "espanol": "contentísimo"
+                },
+                {
+                    "english": "rowdy",
+                    "espanol": "alborotado",
+                    "enDesc": "Agitated, excited, dishevelled."
+                },
+                {
+                    "english": "more rowdy",
+                    "espanol": "más alborotado"
+                },
+                {
+                    "english": "extremely rowdy",
+                    "espanol": "alborotadísimo"
+                },
+                {
+                    "english": "considerate",
+                    "espanol": "considerado"
+                },
+                {
+                    "english": "more considerate",
+                    "espanol": "más considerado"
+                },
+                {
+                    "english": "extremely considerate",
+                    "espanol": "consideradísimo"
+                },
+                {
+                    "english": "obvious",
+                    "espanol": "obvio"
+                },
+                {
+                    "english": "more obvious",
+                    "espanol": "más obvio"
+                },
+                {
+                    "english": "very obvious",
+                    "espanol": "obvísimo"
+                },
+                {
+                    "english": "new",
+                    "espanol": "nuevo"
+                },
+                {
+                    "english": "newer",
+                    "espanol": "más nuevo"
+                },
+                {
+                    "english": "newest",
+                    "espanol": "novísimo"
+                },
+                {
+                    "english": "old",
+                    "espanol": "viejo"
+                },
+                {
+                    "english": "older",
+                    "espanol": "más viejo"
+                },
+                {
+                    "english": "very old",
+                    "espanol": "viejísimo"
+                },
+                {
+                    "english": "ancient",
+                    "espanol": "antiguo"
+                },
+                {
+                    "english": "oldest",
+                    "espanol": "más antiguo"
+                },
+                {
+                    "english": "ancient",
+                    "espanol": "antiquísimo"
+                },
+                {
+                    "english": "faithful",
+                    "espanol": "fiel"
+                },
+                {
+                    "english": "more faithful",
+                    "espanol": "más fiel"
+                },
+                {
+                    "english": "most faithful",
+                    "espanol": "fidelísimo"
+                },
+            ]
+        },
+        {
+            "name": "adjectives 4",
+            "words": [
+                { "english": "neat", "espanol": "pulcro", "engDec":"Neat, tidy." },
+                { "english": "neater", "espanol": "más pulcro" },
+                { "english": "immaculate", "espanol": "pulquérrimo" },
+
+                { "english": "kind", "espanol": "amable" },
+                { "english": "kinder", "espanol": "más amable" },
+                { "english": "very kind", "espanol": "amabilísimo" },
+
+                { "english": "hateful", "espanol": "odioso" },
+                { "english": "more hateful", "espanol": "más odioso" },
+                { "english": "extremely hateful", "espanol": "odiosísimo" },
+
+                { "english": "aggressive", "espanol": "agresivo" },
+                { "english": "more aggressive", "espanol": "más agresivo" },
+                { "english": "very aggressive", "espanol": "agresivisimo" },
+
+                { "english": "docile", "espanol": "docil" },
+                { "english": "more docile", "espanol": "más dócil" },
+                { "english": "very docile", "espanol": "docilísimo" },
+
+                { "english": "free", "espanol": "libre" },
+                { "english": "freer", "espanol": "más libre" },
+                { "english": "very free", "espanol": "librisimo" },
+
+                { "english": "arid", "espanol": "árido" },
+                { "english": "more arid", "espanol": "más árido" },
+                { "english": "extremely arid", "espanol": "aridísimo" },
+
+                { "english": "fertile", "espanol": "fértil", "enDesc": "For soil, land, or a person. Productive." },
+                { "english": "more fertile", "espanol": "más fértil" },
+                { "english": "very fertile", "espanol": "fertilísimo" },
+
+                { "english": "dirty", "espanol": "sucio" },
+                { "english": "dirtier", "espanol": "más sucio" },
+                { "english": "very dirty", "espanol": "sucísimo" },
+
+                { "english": "clean", "espanol": "limpio" },
+                { "english": "cleaner", "espanol": "más limpio" },
+                { "english": "very clean", "espanol": "limpísimo" },
+
+                { "english": "dry", "espanol": "seco" },
+                { "english": "drier", "espanol": "más seco" },
+                { "english": "very dry", "espanol": "sequísimo" },
+
+                { "english": "wet", "espanol": "mojado" },
+                { "english": "wetter", "espanol": "más mojado" },
+                { "english": "very wet", "espanol": "mojadísimo" },
+
+                { "english": "humid/damp", "espanol": "húmedo" },
+                { "english": "more humid", "espanol": "más húmedo" },
+                { "english": "very humid", "espanol": "humidísimo" },
+
+                { "english": "narrow", "espanol": "angosto" },
+                { "english": "narrower", "espanol": "más angosto" },
+                { "english": "very narrow", "espanol": "angostísimo" },
+
+                { "english": "wide", "espanol": "amplio" },
+                { "english": "wider", "espanol": "más amplio" },
+                { "english": "very wide", "espanol": "amplisimo" },
+
+                { "english": "hard", "espanol": "duro" },
+                { "english": "harder", "espanol": "más duro" },
+                { "english": "very hard", "espanol": "durísimo" },
+
+                { "english": "soft", "espanol": "blando" },
+                { "english": "softer", "espanol": "más blando" },
+                { "english": "very soft", "espanol": "blandísimo" },
+
+                { "english": "rigid", "espanol": "rígido" },
+                { "english": "more rigid", "espanol": "más rígido" },
+                { "english": "very rigid", "espanol": "rigidísimo" },
+
+                { "english": "fragile", "espanol": "frágil" },
+                { "english": "more fragile", "espanol": "más frágil" },
+                { "english": "very fragile", "espanol": "fragilísimo" },
+
+                { "english": "bright", "espanol": "briante" },
+                { "english": "brighter", "espanol": "más brillante" },
+                { "english": "very bright", "espanol": "brillantísimo" },
+
+                { "english": "dark", "espanol": "oscurola" },
+                { "english": "darker", "espanol": "más oscuro" },
+                { "english": "very dark", "espanol": "oscurísimo" },
+
+                { "english": "original", "espanol": "original" },
+                { "english": "more original", "espanol": "más original" },
+                { "english": "very original", "espanol": "originalísimo" },
+
+                { "english": "regular", "espanol": "regular" },
+                { "english": "more regular", "espanol": "más regular" },
+                { "english": "very regular", "espanol": "regularísimo" },
+
+                { "english": "intelligent", "espanol": "inteligente" },
+                { "english": "more intelligent", "espanol": "más inteligente" },
+                { "english": "very intelligent", "espanol": "inteligentísimo" },
+
+                { "english": "young", "espanol": "joven" },
+                { "english": "younger", "espanol": "más joven" },
+                { "english": "very young", "espanol": "jovencísimo" },
+
+                { "english": "lively/alive", "espanol": "vivo" },
+                { "english": "more lively", "espanol": "más vivo" },
+                { "english": "very lively", "espanol": "vivísimo" },
+
+                { "english": "dead", "espanol": "muerto" },
+                { "english": "more dead", "espanol": "más muerto" },
+                { "english": "very dead", "espanol": "muertísimo" }
+            ]
         }
     ]
